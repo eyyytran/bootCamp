e8959b7f9f0715e2e69c1918469a3a4baaa52913
eyyytran <ltatran94@gmail.com>
2022-06-13 16:37:17 -0400

    Update homework.py

--- week2/day1/homework.py
+++ week2/day1/homework.py
@@ -9,26 +9,23 @@ class toDo:
         print(f"")
         {self.task}
         {self.priority}
-    # def __eq__(self, other):
-    #     return self.priority == other.priority
-    # def __ls__(self, other):
-    #     return self.priority > other.priority
-    
 
 def addTask():
     userTask = input('Type in your task here: ')
-    taskPriority = input('Add a Priority:\n 1 = Low\n 2 = Medium\n 3 = High\n ')
+    taskPriority = input('Add a Priority:\nLow\nMedium\nHigh\n')
     newTask = toDo(userTask, taskPriority)
     taskList.append(newTask)
 
 def removeTask():
-    userRemove = int(input('Which task do you want to remove?\n'))-1
+    userRemove = int(input('Which task do you want to remove?\n'))
     for task in taskList:
-        del taskList[userRemove]
+        taskList.pop(userRemove)
 
 def printTasks():
     for toDo in taskList:
-        print(toDo.priority + ' - ' + toDo.task)
+        index = taskList.index(toDo)
+        print(str(index+1) + ' - ' + toDo.task + ' - ' + toDo.priority)
+
 
 #adding to to-do list
 def toDos():
@@ -37,10 +34,10 @@ def toDos():
         userOption = input("Type 1 to Add to the List\nType 2 to Remove a Task\nType 3 to Print List\nType 4 to Exit\n")
         if userOption == '1':
             addTask()
-        if userOption == '2':
+        elif userOption == '2':
             removeTask()
-        if userOption == '3':
+        elif userOption == '3':
             printTasks()
-        if userOption == '4':
+        elif userOption == '4':
             runList = False
 toDos()
