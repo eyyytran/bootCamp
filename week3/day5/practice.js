5e3bfa38de96e58650659f3cc9a3f85f3bfe20e8
eyyytran <ltatran94@gmail.com>
2022-06-24 18:39:47 -0400

    classwork

---
+++ week3/day5/practice.js
@@ -0,0 +1,24 @@
+const restaurants = ['umaido', 'raku', 'jangsujang']
+// console.log(restaurants[restaurants.length - 1])
+
+for (let index = 0; index < restaurants.length; index++) {
+    const restaurant = restaurants[index]
+    console.log(restaurant)
+}
+
+for (let restaurant of restaurants) {
+    console.log(restaurant)
+}
+
+for (let key in Object) {
+}
+
+function TwoSum() {
+    let name = 'rayleigh'
+    name = "it's still rayleigh"
+    return "i'm gonna miss her"
+}
+
+console.log(TwoSum())
+
+const ThreeSum = () => {}
