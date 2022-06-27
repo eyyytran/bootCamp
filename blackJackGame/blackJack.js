4b137ab469860518b08a3641f0d77dbd4b70d83b
eyyytran <ltatran94@gmail.com>
2022-06-27 17:16:48 -0400

    makeDeck function

--- blackJackGame/blackJack.js
+++ blackJackGame/blackJack.js
@@ -1,11 +1,25 @@
 var gameState = {
-    gameStatus : 'init', //'init' or 'exit'
-    playerHand : [],
-    dealerHand : [],
-    gameDeck : [],
-    gameResult : null
+    gameStatus: 'init', //'init' or 'exit'
+    playerHand: [],
+    dealerHand: [],
+    gameDeck: [],
+    gameResult: null,
 }
 //make a function that creates a deck of cards
-const makeDeck () => {
-
+const makeDeck = () => {
+    cards = []
+    let suits = ['H', 'S', 'C', 'D']
+    let values = [2, 3, 4, 5, 6, 7, 8, 9]
+    for (let suitCounter = 0; suitCounter < suits.length; suitCounter++) {
+        for (valueCounter = 0; valueCounter < values.length; valueCounter++) {
+            let card = {
+                suit: suits[suitCounter],
+                value: values[[valueCounter]],
+            }
+            cards.push(card)
+        }
+    }
+    console.log(cards)
 }
+
+makeDeck()
