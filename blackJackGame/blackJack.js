2723c55bcde65861daa970599c63dfb798acbd67
eyyytran <ltatran94@gmail.com>
2022-06-27 16:29:41 -0400

    creation of black jack project

---
+++ blackJackGame/blackJack.js
@@ -0,0 +1,11 @@
+var gameState = {
+    gameStatus : 'init', //'init' or 'exit'
+    playerHand : [],
+    dealerHand : [],
+    gameDeck : [],
+    gameResult : null
+}
+//make a function that creates a deck of cards
+const makeDeck () => {
+
+}
