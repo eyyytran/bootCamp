a67b6140e824d25f5f753446bbb7db7531d84852
eyyytran <ltatran94@gmail.com>
2022-06-28 12:33:29 -0400

    manipulating DOM

--- week4/day2/app.js
+++ week4/day2/app.js
@@ -1,14 +1,45 @@
 console.log(document)
 //
-const button1 = parseInt(document.getElementById('button1').innerText)
+const numbersToCalc = []
+let answer = ''
+const sum = (total) => {
+    console.log('this is the sum', total)
+    numbersToCalc.reduce()
+}
+
+const getButtonValue = (buttonInnerText) => {
+    if (buttonInnerText.id === 'addition') {
+        let mathSign = buttonInnerText.innerText
+        numbersToCalc.push(mathSign)
+    } else {
+        const innerTextToInt = parseInt(buttonInnerText.innerText)
+        numbersToCalc.push(innerTextToInt)
+    }
+}
+
+const textbox = document.getElementById('textbox')
+
+const doMaff = () => {
+    const firstVal = numbersToCalc[0]
+    const mathSign = numbersToCalc[1]
+    const secondVal = numbersToCalc[2]
+    if (mathSign === '+') {
+        answer = firstVal + secondVal
+        textbox.value = answer
+        const bigAnswer = document.createElement('h1')
+        bigAnswer.innerText = 'CowBoy'
+        container.append(bigAnswer)
+    }
+}
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
 const buttons = document.getElementsByTagName('button')
-for (const button of buttons) {
-    console.log(button.innerText)
-}
+const equals = document.getElementById('equals')
+const addition = document.getElementById('addition')
 
-const addition = (number) => {
-    let sum = number + number
-    console.log(sum)
-}
+const container = document.getElementById('container')
 
-addition(button1)
+addition.onclick = (e) => getButtonValue(e.target)
+equals.onclick = doMaff
+button1.onclick = (e) => getButtonValue(e.target)
+button2.onclick = (e) => getButtonValue(e.target)
