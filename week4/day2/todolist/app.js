618046fb853552118f4dca11f4020d0699f906c2
eyyytran <ltatran94@gmail.com>
2022-06-28 15:06:42 -0400

    tasklist

--- week4/day2/todolist/app.js
+++ week4/day2/todolist/app.js
@@ -2,5 +2,13 @@ toDoList = []
 
 const userInput = document.getElementById('input')
 const submitButton = document.getElementById('submit')
+const newItem = document.getElementById('input').value
 
-submitButton.addEventListener('click', () => console.log('button'))
+submitButton.addEventListener('click', () => {
+    const input = document.getElementById('input')
+    const newLi = document.createElement('li')
+    newLi.innerHTML = input.value
+    // let li = '<li>' + input.value + '</li>'
+    const list = document.getElementById('taskList')
+    list.appendChild(newLi)
+})
