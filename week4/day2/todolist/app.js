a20552123d02c09f637591b404edae0b13441150
eyyytran <ltatran94@gmail.com>
2022-06-28 15:53:16 -0400

    create todolist

--- week4/day2/todolist/app.js
+++ week4/day2/todolist/app.js
@@ -1,14 +1,28 @@
-toDoList = []
-
 const userInput = document.getElementById('input')
 const submitButton = document.getElementById('submit')
-const newItem = document.getElementById('input').value
+const input = document.getElementById('input')
+let counter = 1
 
-submitButton.addEventListener('click', () => {
-    const input = document.getElementById('input')
-    const newLi = document.createElement('li')
-    newLi.innerHTML = input.value
-    // let li = '<li>' + input.value + '</li>'
-    const list = document.getElementById('taskList')
-    list.appendChild(newLi)
+const createToDo = () => {
+    const newItem = document.createElement('p')
+    const checkButton = document.createElement('button')
+    const strikeButton = document.createElement('button')
+    const taskList = document.getElementById('task-container')
+    newItem.innerHTML = input.value
+    checkButton.innerHTML = 'Put Check'
+    strikeButton.innerHTML = 'Put X'
+    newItem.classList.add('item')
+    // checkButton.classList.add('checkButton' + toString(counter))
+    // strikeButton.classList.add('strikeButton' + toString(counter))
+    // let counter = counter++
+    newItem.appendChild(checkButton)
+    newItem.appendChild(strikeButton)
+    taskList.appendChild(newItem)
+}
+
+submitButton.addEventListener('click', createToDo)
+
+checkButton.addEventListener('click', () => {
+    const item = document.getElementsByClassName('item')
+    item.innerHTML.strike()
 })
