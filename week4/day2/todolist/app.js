6cb66ddf98f360b0746168ddf6944c380320898a
eyyytran <ltatran94@gmail.com>
2022-06-28 23:09:27 -0400

    refactored css styling

--- week4/day2/todolist/app.js
+++ week4/day2/todolist/app.js
@@ -8,28 +8,35 @@ const createToDo = () => {
     newItem.innerHTML = input.value
     newItem.classList.add('item')
 
-    const checkButton = document.createElement('button')
-    checkButton.innerHTML = '✓'
-    checkButton.classList.add('checkButton')
+    const checkBtn = document.createElement('button')
+    checkBtn.innerHTML = '✓'
+    checkBtn.classList.add('checkButton')
 
-    const strikeButton = document.createElement('button')
-    strikeButton.innerHTML = 'x'
-    strikeButton.classList.add('strikeButton')
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerHTML = 'x'
+    deleteBtn.classList.add('deleteButton')
 
-    newItem.appendChild(checkButton)
-    newItem.appendChild(strikeButton)
+    newItem.appendChild(checkBtn)
+    newItem.appendChild(deleteBtn)
     taskList.appendChild(newItem)
 
     input.value = ''
     input.focus()
 
-    checkButton.addEventListener('click', () => {
-        newItem.style = 'text-decoration:line-through;'
+    checkBtn.addEventListener('click', () => {
+        if (newItem.style['text-decoration'] === '') {
+            newItem.style = 'text-decoration:line-through;'
+        } else {
+            newItem.style = ''
+        }
     })
 
-    strikeButton.addEventListener('click', () => {
+    deleteBtn.addEventListener('click', () => {
         taskList.removeChild(newItem)
     })
 }
 
 submitButton.addEventListener('click', createToDo)
+
+// TODO: make it create a todo when you press enter inside the input
+// TODO: init the page w/ some test todos using optional arg in createToDo function
