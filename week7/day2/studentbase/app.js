d6cfd2d73fd1dd94dac816df4af542669394dd1e
eyyytran <ltatran94@gmail.com>
2022-07-19 15:01:32 -0400

    Update app.js

--- week7/day2/studentbase/app.js
+++ week7/day2/studentbase/app.js
@@ -6,6 +6,17 @@ import { createClient } from '@supabase/supabase-js'
 const app = express()
 const PORT = process.env.PORT || 3000
 const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY)
-console.log(supabase)
+
+app.use(express.json())
+
+app.post('/create_student', async (req, res) => {
+    const { data, error } = await supabase.from('web-06-22').insert([req.body])
+    if (data) {
+        res.send(data)
+    }
+    if (error) {
+        res.send(error)
+    }
+})
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
