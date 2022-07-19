eba3be38dea26b741b3a8c19f0e27ceaf5facf0e
eyyytran <ltatran94@gmail.com>
2022-07-19 14:29:56 -0400

    Update app.js

--- week7/day2/studentbase/app.js
+++ week7/day2/studentbase/app.js
@@ -1,3 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config()
-console.log(process.env.API_KEY)
+import express from 'express'
+import { createClient } from '@supabase/supabase-js'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY)
+console.log(supabase)
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
