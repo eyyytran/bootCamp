084b5b15a6816b93b558ad85cca377cabd17199e
eyyytran <ltatran94@gmail.com>
2022-07-28 10:51:27 -0400

    created sessions

--- week8/day4/controller/index.js
+++ week8/day4/controller/index.js
@@ -1,11 +1,45 @@
 const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const { Users } = require('../sequelize/models')
+const users = require('../sequelize/models/users')
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use(cookieParser())
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        },
+    })
+)
 
-app.get('/login', (req, res) => {
-    res.send('login')
+app.post('/login', async (req, res) => {
+    // console.log(req.session)
+    const user = await Users.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password,
+        },
+    })
+    if (user) {
+        req.session.user = user
+        // console.log(req.session)
+        res.json({
+            message: 'Login Success',
+            user: user,
+        })
+    } else {
+        res.json({
+            message: 'Login Failed',
+        })
+    }
 })
 
-app.listen(PORT, console.log(`listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
