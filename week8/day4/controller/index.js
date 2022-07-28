8748ce188687123982fba8214607eec64cca2686
eyyytran <ltatran94@gmail.com>
2022-07-28 15:07:42 -0400

    cookies

--- week8/day4/controller/index.js
+++ week8/day4/controller/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
-const { Users } = require('../sequelize/models')
+const es6Renderer = require('express-es6-template-engine')
+const { Users, Sessions } = require('../sequelize/models')
+const models = require('../sequelize/models')
 const users = require('../sequelize/models/users')
 const app = express()
 const PORT = 3000
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const store = new SequelizeStore({ db: models.sequelize })
+
+app.engine('html', es6Renderer)
+app.set('views', 'views')
+app.set('view session', 'html')
 
 app.use(express.json())
 app.use(cookieParser())
@@ -13,19 +21,25 @@ app.use(
         secret: 'secret',
         resave: false,
         saveUninitialized: true,
+        store: store,
         cookie: {
-            secure: false,
-            maxAge: 1000 * 60 * 60 * 24 * 7,
+            maxAge: 60 * 60 * 1000 * 24 * 7,
         },
     })
 )
+store.sync()
 
 const checkLogin = (req, res, next) => {
-    if (req.session.user) {
-        next() //literally says thank you next
+    const sessionValid = Sessions.findOne({
+        where: {
+            sid: req.session.session.sid,
+        },
+    })
+    if (sessionValid) {
+        next()
     } else {
         res.json({
-            message: 'You need to be logged in.',
+            message: 'login failed',
         })
     }
 }
@@ -54,6 +68,10 @@ app.post('/login', async (req, res) => {
 
 app.post('/delete_all_secrets', checkLogin, async (req, res) => {
     res.send('Omg you deleted everything...')
+})
+
+app.get('/home', checkLogin, (req, res) => {
+    res.render('index', { locals: { name: req.session.username } })
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
---
+++ week8/day4/sequelize/migrations/20220728162348-create-sessions.js
@@ -0,0 +1,33 @@
+'use strict'
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Sessions', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            sid: {
+                type: Sequelize.INTEGER,
+            },
+            expires: {
+                type: Sequelize.DATE,
+            },
+            data: {
+                type: Sequelize.TEXT,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Sessions')
+    },
+}
