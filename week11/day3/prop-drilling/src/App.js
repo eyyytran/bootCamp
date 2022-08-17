585c72cfa76a7525303f9b8b6dffd65cdc8df15c
eyyytran <ltatran94@gmail.com>
2022-08-17 17:14:04 -0400

    add pokemon by id or name

--- week11/day3/prop-drilling/src/App.js
+++ week11/day3/prop-drilling/src/App.js
@@ -4,19 +4,14 @@ import { PokemonCard } from './components/PokemonCard'
 
 function App() {
     const [pokemonList, setPokemonList] = useState([])
-    const [query, setQuery] = useState('')
-    const [initialPokemonList, setInitialPokemonList] = useState([])
-
-    useEffect(() => {
-        if (query === '') setPokemonList(initialPokemonList)
-    }, [query])
+    const [searchQuery, setSearchQuery] = useState('')
+    const [addQuery, setAddQuery] = useState('')
 
     useEffect(() => {
         const fetchPokemon = async () => {
             const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
             const json = await response.json()
             setPokemonList(json.results)
-            setInitialPokemonList(json.results)
         }
         fetchPokemon()
     }, [])
@@ -29,22 +24,30 @@ function App() {
         )
     }
 
-    const searchPokemon = query => {
-        setPokemonList(
-            pokemonList.filter(pokemon => {
-                return pokemon.name.includes(query)
-            })
-        )
-    }
-
-    const handleChange = e => {
-        setQuery(e.target.value.toLowerCase())
-    }
-
     const handleSearch = e => {
+        setSearchQuery(e.target.value.toLowerCase())
+    }
+
+    const handleAdd = e => {
+        setAddQuery(e.target.value.toLowerCase())
+    }
+
+    const handleSubmitAddQuery = async e => {
         e.preventDefault()
-        if (query !== '') {
-            searchPokemon(query)
+        if (addQuery === '') return
+        try {
+            const response = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${addQuery}`
+            )
+            const json = await response.json()
+            const newPokemon = {
+                name: json.name,
+            }
+            setPokemonList([newPokemon, ...pokemonList])
+            setAddQuery('')
+        } catch (error) {
+            console.log('this pokemon was not found')
+            setAddQuery('')
         }
     }
 
@@ -52,19 +55,29 @@ function App() {
         <div className='App'>
             <div className='header'>
                 <h1>Twenty Pokemon</h1>
-                <form onSubmit={handleSearch}>
+            </div>
+            <div className='nav-bar'>
+                <input
+                    type='text'
+                    name='search'
+                    placeholder='Search'
+                    value={searchQuery}
+                    onChange={handleSearch}
+                />
+                <form onSubmit={handleSubmitAddQuery}>
                     <input
                         type='text'
-                        name='user-input'
-                        value={query}
-                        onChange={handleChange}
+                        name='add'
+                        placeholder='Name or Dex#'
+                        value={addQuery}
+                        onChange={handleAdd}
                     />
-                    <button type='submit'>Search</button>
+                    <button type='submit'>+</button>
                 </form>
             </div>
             <div className='card-container'>
                 {pokemonList
-                    // .filter(pokemon => pokemon.name.includes(query))
+                    .filter(pokemon => pokemon.name.includes(searchQuery))
                     .map(pokemon => (
                         <PokemonCard
                             pokemon={pokemon}
