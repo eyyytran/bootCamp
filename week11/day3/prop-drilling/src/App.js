52691785001054618c2d902c8f96c1faf7ec028a
eyyytran <ltatran94@gmail.com>
2022-08-17 13:18:02 -0400

    added search function

--- week11/day3/prop-drilling/src/App.js
+++ week11/day3/prop-drilling/src/App.js
@@ -4,12 +4,19 @@ import { PokemonCard } from './components/PokemonCard'
 
 function App() {
     const [pokemonList, setPokemonList] = useState([])
+    const [query, setQuery] = useState('')
+    const [initialPokemonList, setInitialPokemonList] = useState([])
+
+    useEffect(() => {
+        if (query === '') setPokemonList(initialPokemonList)
+    }, [query])
 
     useEffect(() => {
         const fetchPokemon = async () => {
             const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
             const json = await response.json()
             setPokemonList(json.results)
+            setInitialPokemonList(json.results)
         }
         fetchPokemon()
     }, [])
@@ -22,18 +29,48 @@ function App() {
         )
     }
 
+    const searchPokemon = query => {
+        setPokemonList(
+            pokemonList.filter(pokemon => {
+                return pokemon.name.includes(query)
+            })
+        )
+    }
+
+    const handleChange = e => {
+        setQuery(e.target.value.toLowerCase())
+    }
+
+    const handleSearch = e => {
+        e.preventDefault()
+        if (query !== '') {
+            searchPokemon(query)
+        }
+    }
+
     return (
         <div className='App'>
             <div className='header'>
                 <h1>Twenty Pokemon</h1>
+                <form onSubmit={handleSearch}>
+                    <input
+                        type='text'
+                        name='user-input'
+                        value={query}
+                        onChange={handleChange}
+                    />
+                    <button type='submit'>Search</button>
+                </form>
             </div>
             <div className='card-container'>
-                {pokemonList.map(pokemon => (
-                    <PokemonCard
-                        pokemon={pokemon}
-                        deletePokemon={deletePokemon}
-                    />
-                ))}
+                {pokemonList
+                    // .filter(pokemon => pokemon.name.includes(query))
+                    .map(pokemon => (
+                        <PokemonCard
+                            pokemon={pokemon}
+                            deletePokemon={deletePokemon}
+                        />
+                    ))}
             </div>
         </div>
     )
