3afac40a7036f0eb97bd1aab4891d20d3a6b043c
eyyytran <ltatran94@gmail.com>
2022-08-23 16:44:39 -0400

    moving things around

--- week12/day2/redux-day-3v2/src/App.tsx
+++ week12/day2/redux-day-3v2/src/App.tsx
@@ -1,10 +1,7 @@
-import { useAppSelector, useAppDispatch } from './app/hooks'
 import './App.css'
 import Homepage from './components/Homepage'
 
 function App() {
-    const dispatch = useAppDispatch()
-
     return (
         <div className='App'>
             <h1>Andrea Store</h1>
